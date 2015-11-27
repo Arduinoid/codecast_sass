65cf13646f594aa7656ba744fe728c8fd25a9ae1
Jonathon Pinnow <pinnowj@gmail.com>
2015-11-27 18:03:22 +0000

    Made improvements to user profile page

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+    def job_title_icon
+        title = @user.profile.job_title
+        if title == "Developer"
+            "<i class='fa fa-code'></i>".html_safe
+        elsif title == "Entrepreneur"
+            "<i class='fa fa-lightbulb-o'></i>".html_safe
+        elsif title == "Investor"
+            "<i class='fa fa-dollar'></i>".html_safe
+        
+        end
+    end
+end
